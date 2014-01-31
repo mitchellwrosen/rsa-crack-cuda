f33e5fcc1375e642904f807062ef6ec34a36a0a2
Mitchell Rosen <mitchellwrosen@gmail.com>
2014-01-31 10:33:33 -0800

    remove printf

--- sequential/main.c
+++ sequential/main.c
@@ -15,11 +15,10 @@ int main(int argc, char** argv) {
   mpz_t gcd;
   mpz_init(gcd);
   for (int i = 0; i < numKeys-1; ++i) {
-    fprintf(stdout, "%d/%d\n", i, numKeys-2);
     for (int j = i+1; j < numKeys; ++j) {
       mpz_gcd(gcd, keys[i], keys[j]);
       if (mpz_cmp_ui(gcd, 1) != 0)
-        gmp_fprintf(stdout, "%d,%d: %Zd\n", i, j, gcd);
+        gmp_fprintf(stdout, "#%d,#%d: %Zd\n", i, j, gcd);
     }
   }
 }
